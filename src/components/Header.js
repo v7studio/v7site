2ce7f1b3b5b0e3cec97bc6e72aca1f58ef931eff
v7studio <v7studio007@gmail.com>
2024-09-30 13:48:19 +0530

    design updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 
@@ -17,42 +16,34 @@ const Header = () => {
   };
 
   return (
-    <HelmetProvider>
-      <Helmet>
-        <title>V7Studio Photography</title>
-        <meta name="description" content="V7Studio Photography - Capturing Life's Moments in Timeless Art" />
-        <link rel="canonical" href="http://yourwebsite.com/" />
-      </Helmet>
-
-      <header className="header">
-        <div className="logo">
-          <Link to="/" onClick={closeMenu}>
-            <img src={Logo} alt="VStudio Logo" />
-          </Link>
-        </div>
-        <div className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-        <nav className={`navigation ${isMenuOpen ? 'open' : ''}`}>
-          <ul>
-            <li><Link to="/" className="active" onClick={closeMenu}>Home</Link></li>
-            <li><Link to="/services" onClick={closeMenu}>Services</Link></li>
-            <li><Link to="/about" onClick={closeMenu}>About Us</Link></li>
-            <li><Link to="/portfolio" onClick={closeMenu}>Portfolio</Link></li>
-            <li><Link to="/gallery" onClick={closeMenu}>Gallery</Link></li>
-            <li><Link to="/contact" onClick={closeMenu}>Contact Us</Link></li>
-          </ul>
-        </nav>
-        <div className="header-buttons">
-          <a href="https://www.instagram.com/v7_studio__" target="_blank" rel="noopener noreferrer" className="instagram-icon">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <Link to="/contact" className="hire-us-button">Hire Us</Link>
-        </div>
-      </header>
-    </HelmetProvider>
+    <header className="header">
+      <div className="logo">
+        <Link to="/" onClick={closeMenu}>
+          <img src={Logo} alt="VStudio Logo" />
+        </Link>
+      </div>
+      <div className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <nav className={`navigation ${isMenuOpen ? 'open' : ''}`}>
+        <ul>
+          <li><Link to="/" className="active" onClick={closeMenu}>Home</Link></li>
+          <li><Link to="/services" onClick={closeMenu}>Services</Link></li>
+          <li><Link to="/about" onClick={closeMenu}>About Us</Link></li>
+          <li><Link to="/portfolio" onClick={closeMenu}>Portfolio</Link></li>
+          <li><Link to="/gallery" onClick={closeMenu}>Gallery</Link></li>
+          <li><Link to="/contact" onClick={closeMenu}>Contact Us</Link></li>
+        </ul>
+      </nav>
+      <div className="header-buttons">
+        <a href="https://www.instagram.com/v7_studio__" target="_blank" rel="noopener noreferrer" className="instagram-icon">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <Link to="/contact" className="hire-us-button">Hire Us</Link>
+      </div>
+    </header>
   );
 }
 
